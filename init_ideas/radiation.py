ddf68fb9879162c6c9c57792fbb08a3307e3bb76
ilaria-manco <32579635+ilaria-manco@users.noreply.github.com>
2017-10-17 11:20:12 +0100

    Separate classes

---
+++ init_ideas/radiation.py
@@ -0,0 +1,18 @@
+from utils import disp
+import numpy as np
+from init_models import mob_model
+
+class radiation(mob_model):
+	'''
+	The normalised radiation human mobility model
+	'''
+	def flux(self, i, j):
+		'''
+		Takes the indices of two locations i, j and returns the average flux from i to j
+		'''
+		pop = self.pop
+		popi, popj = pop.popDist[i], pop.popDist[j]
+		popSij = pop.s(i, j)
+		n = (popi/(1-popi/pop.M()))*(popi*popj)/float((popi+popSij)*(popi+popSij+popj)) # TODO how do we define Ti here?
+
+		return n
