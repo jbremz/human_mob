a23d7888c7aa454f904364d2363b47d41f25a5be
jbremz <jhbremner@gmail.com>
2017-10-17 14:32:12 +0100

    added setup for pip install

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='msci_human_mob_project',
+    version='0.1',
+    long_description='Msci Human Mobility Project',
+    packages=find_packages(),
+    install_requires=[],
+)
