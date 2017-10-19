27b33b23b31ccb504b8c06e57bf53bdf1a77451c
jbremz <jhbremner@gmail.com>
2017-10-19 11:44:15 +0100

    Restructured population distributions
    Also added random and explicit classes

---
+++ hm/pop_models/pop_random.py
@@ -0,0 +1,32 @@
+from hm.pop_models.population import pop_distribution
+import numpy as np
+
+class random(pop_distribution):
+	'''
+	Random population distribution of N locations in 2D space (0<x<1, 0<y<1), and uniform population distribution of length N
+
+	TODO: random size distribution also?
+
+	'''
+
+	def __init__(self, N, uniform=True):
+		self.N = N
+		self.uniform = uniform
+		super().__init__()
+
+	def pop_dist(self):
+		'''
+		Returns the population distribution for N locations
+
+		TODO: non-uniform
+
+		'''
+		if self.uniform:
+			return np.ones(self.N)
+
+	def loc_dist(self):
+		'''
+		Returns the random 2D population distribution: N locations (0<x<1, 0<y<1)
+
+		'''
+		return np.random.rand(self.N,2)
