a08382964ee320f182fa3b06e8b404a52c209caa
jbremz <jhbremner@gmail.com>
2017-10-24 13:12:58 +0100

    explicit_tripoint

---
+++ hm/analysis/explicit_tripoint.py
@@ -0,0 +1,38 @@
+from hm.pop_models.pop_random import random as pop_random
+from hm.hm_models.gravity import gravity
+import numpy as np
+import copy
+
+def epsilon(x,y,N, size=1.):
+	'''
+	Takes the x and y displacements defined in the tripoint problem and returns
+	the error between treating the satellite locations as one and as separate
+
+	'''
+	p = pop_random(N)
+
+	loci = [0.5+x/2., 0.5]
+	locj = [0.5-x/2., 0.5-y/2.]
+	lock = [0.5-x/2., 0.5+y/2.]
+	locb = [0.5-x/2., 0.5]
+
+	sizeb = 2*size # TODO change to non-uniform size
+
+	p3 = copy.deepcopy(p) # the tripoint arrangement
+	p2 = copy.deepcopy(p) # the two-point arrangement
+
+	p3.locCoords = np.insert(p3.locCoords, 0, np.array([loci, locj, lock]), axis=0)
+	p2.locCoords = np.insert(p2.locCoords, 0, np.array([loci, locb]), axis=0)
+	p3.popDist = np.insert(p3.popDist, 0, np.array([size, size, size]), axis=0)
+	p2.popDist = np.insert(p2.popDist, 0, np.array([size, sizeb]), axis=0)
+
+	g3 = gravity(p3, 1, 1, 2)
+	g2 = gravity(p2, 1, 1, 2)
+
+	eps = g2.flux(0,1) - (g3.flux(0,1)+g3.flux(0,2))
+
+	return np.array(eps)
+
+
+
+
