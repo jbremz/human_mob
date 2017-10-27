bc3882256f054557efbb4a8b234a07b3f24f89ca
jbremz <jhbremner@gmail.com>
2017-10-27 17:16:21 +0100

    reverse flux doesn't work

--- hm/analysis/explicit_tripoint.py
+++ hm/analysis/explicit_tripoint.py
@@ -6,10 +6,12 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import FormatStrFormatter
 import copy
 
-def epsilon(x,y,N, size=1.):
+def epsilon(x,y,N, size=1., ib=True):
 	'''
 	Takes the x and y displacements defined in the tripoint problem and returns
 	the error between treating the satellite locations as one and as separate
+	ib = True --> calculate Tib 
+	ib = False --> calulate Tbi (i.e. flow in opposite direction)
 
 	'''
 	p = pop_random(N)
@@ -29,18 +31,23 @@ def epsilon(x,y,N, size=1.):
 	p3.popDist = np.insert(p3.popDist, 0, np.array([size, size, size]), axis=0)
 	p2.popDist = np.insert(p2.popDist, 0, np.array([size, sizeb]), axis=0)
 
-	g3 = gravity(p3, 1, 1, 2)
+	g3 = gravity(p3, 1, 1, 2) # TODO Gamma=2, try other exponents and other functions?
 	g2 = gravity(p2, 1, 1, 2)
 
-	g2Flux = g2.flux(0,1)
+	if ib: # From i to the satellites
+		g2Flux = g2.flux(0,1)
+		eps = (g2Flux - (g3.flux(0,1)+g3.flux(0,2)))/g2Flux
 
-	eps = (g2Flux - (g3.flux(0,1)+g3.flux(0,2)))/g2Flux
+	# TODO This doesn't work
+	else: # from the satellites to i 
+		g2Flux = g2.flux(1,0)
+		eps = (g2Flux - (g3.flux(1,0)+g3.flux(2,0)))/g2Flux
 
 	return np.array(eps)
 
 
 
-def anaTP(xmin, xmax, ymin, ymax, n, N):
+def anaTP(xmin, xmax, ymin, ymax, n, N, ib=True):
 	'''
 	Finds values of epsilon in an nxn 2D sample space for x and y at fixed N random locations
 
@@ -56,13 +63,13 @@ def anaTP(xmin, xmax, ymin, ymax, n, N):
 
 	for j, row in enumerate(xy):
 		for i, pair in enumerate(row):
-			epsVals[j][i] = epsilon(pair[0], pair[1], N)
+			epsVals[j][i] = epsilon(pair[0], pair[1], N, ib=ib)
 
 	nanMask = np.isnan(epsVals)
 
 	epsVals[nanMask] = 1
 
-	epsVals = np.flip(epsVals, 0)
+	epsVals = np.flip(epsVals, 0) # make the y axis ascend
 
 	xticks = np.around(x*np.sqrt(N), 2)
 	yticks = np.flip(np.around(y*np.sqrt(N), 2), 0)
@@ -77,7 +84,7 @@ def anaTP(xmin, xmax, ymin, ymax, n, N):
 
 	plt.show()
 
-	return xy, epsVals
+	return
 
 
 
