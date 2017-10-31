86b8f95e4e847a905f3648171d3365a77c1337a0
ilaria-manco <32579635+ilaria-manco@users.noreply.github.com>
2017-10-31 16:45:23 +0000

    random tripoint

--- hm/analysis/random_tripoint.py
+++ hm/analysis/random_tripoint.py
@@ -1,15 +1,22 @@
 from hm.hm_models.gravity import gravity
-from hm.hm_models.radiation import radiation
-from hm.hm_models.opportunities import opportunities
 from hm.pop_models.pop_random import random as pop_random
+import numpy as np
+import seaborn as sns; sns.set()
+import matplotlib.pyplot as plt
+import copy
+from hm.utils.utils import disp
+from scipy import interpolate
 
-N = 20
+
+N = 100
 alpha, beta = 1, 1
 gamma = 0.2
 p = pop_random(N)
 g = gravity(p, alpha, beta, gamma)
 
-def neighbours():
+def neighbours(k):
+	'''Returns a list of nearest neighobours pairs as a Numpy array.'''
+
 	neighbours = []
 	for i in range(p.size):
 		distance = []
@@ -20,13 +27,67 @@ def neighbours():
 		if i < j:
 			neighbours.append([i, n+1])
 		if i > j:
-			neighbours.append([i, n+1])
-	return np.array(neighbours)
+			neighbours.append([i, n])
+	return np.array(neighbours[1:])
+
+def neighbours_dist(k):
+	'''
+	Returns the distance between two nearest neighbours as a Numpy array.
+	The pair's index matches that of neighbours().
+	'''
+
+	distance = []
+	for i in neighbours(k):
+		distance.append(p.r(i[0], i[1]))
+
+	return np.array(distance)
+
+def target_dist(i):
+	'''
+	Returns the distance between the midpoint between two nearest neighbours and the
+	target location i as a Numpy array.
+	The pair's index matches that of neighbours().
+	'''
+
+	r = []
+	for n in neighbours(i):
+		x = (p.locCoords[n[0]][0]+ p.locCoords[n[1]][0])*0.5
+		y = (p.locCoords[n[0]][1]+ p.locCoords[n[1]][1])*0.5
+		r.append(disp(p.locCoords[i], np.array([x, y])))
+	return np.array(r)
 
 
-def eps():
-	for i in range(p.size):
-		for b in neighbours():
-			j, k = b[0], b[1]
-			if p.r(j, k) <= 0.5*np.sqrt(p.r(i, b[0]):
-				print(i, j, k)
+def epsilon(i):
+	'''Using abs!!!'''
+	epsValues = []
+	for n in neighbours(i):
+		j, k = n[0], n[1]
+		p2 = copy.deepcopy(p)
+		if p2.locCoords[j][1] - p2.locCoords[k][1] > 0:
+			p2.locCoords[j][1] = p2.locCoords[j][1] - 0.5*(p2.locCoords[j][1]+ p2.locCoords[k][1])
+		else:
+			p2.locCoords[j][1] = p2.locCoords[j][1] + 0.5*(p2.locCoords[j][1]+ p2.locCoords[k][1])
+		if p2.locCoords[j][0] - p2.locCoords[k][0] > 0:
+			p2.locCoords[j][0] = p2.locCoords[j][0] - 0.5*(p2.locCoords[j][0]+ p2.locCoords[k][0])
+		else:
+			p2.locCoords[j][0] = p2.locCoords[j][0] + 0.5*(p2.locCoords[j][0]+ p2.locCoords[k][0])
+		p2.locCoords[k][1] = p2.locCoords[j][1]
+		p2.locCoords[k][0] = p2.locCoords[j][0]
+		g2 = gravity(p2, alpha, beta, gamma)
+		eps = ((g2.flux(i, j) + g2.flux(i,k)) - (g.flux(i, j)+g.flux(i, k)))/(g2.flux(i, j) + g2.flux(i,k))
+		epsValues.append(abs(eps))
+	return np.array(epsValues)
+
+
+def heatmap(i):
+	x = target_dist(i)
+	y = neighbours_dist(i)
+	epsVals = epsilon(i)
+	xy = np.column_stack((x, y))
+
+	f = interpolate.griddata(xy, epsVals, (np.linspace(0,1, 0.1), np.linspace(0, 1, 0.1)), method = 'nearest')
+
+	ax = sns.heatmap(epsVals)
+	plt.show()
+	return x, y
+
