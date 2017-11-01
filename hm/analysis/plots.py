dcccfee4cbcdc8e3fb9e2249948c4645ed996049
ilaria-manco <32579635+ilaria-manco@users.noreply.github.com>
2017-11-01 14:35:38 +0000

    reorganised plot into function

--- hm/analysis/plots.py
+++ hm/analysis/plots.py
@@ -5,32 +5,46 @@ from hm.pop_models.pop_random import random as pop_random
 from matplotlib import pyplot as plt
 import numpy as np
 
-alpha, beta = 1, 1
-gamma = 0.2
-N = 30
+def plot_flow(population, model='all', alpha = 1, beta = 1, gamma = 0.2):
 
-p = pop_random(N)
-g = gravity(p, alpha, beta, gamma)
-r = radiation(p)
-o = opportunities(p, gamma)
+	'''
+	Takes a population object and a mobility model and plots the flow probability
+	as a function of the (scaled) distance between two locations
 
-distance = []
-flux_gravity = []
-flux_radiation = []
-flux_opportunities = []
-for i in range(p.size):
-	for j in range(p.size):
-		if i != j:
-			distance.append(p.r(i, j)/np.sqrt(N))
-			flux_gravity.append(g.flux(i, j))
-			flux_radiation.append(r.flux(i, j))
-			flux_opportunities.append(o.flux(i, j))
+	TODO: pass instance of mob_model as second argument
+	'''
+	if model != 'all':
+		isinstance (model, mob_model)
 
-plt.loglog(distance, flux_gravity, '.',markersize = 3, label = 'Gravity')
-plt.loglog(distance, flux_radiation, '.', markersize = 3,label = 'Radiation')
-plt.loglog(distance, flux_opportunities, '.', markersize = 3, label = 'Opportunities')
+	distance = []
+	flux_gravity = []
+	flux_radiation = []
+	flux_opportunities = []
+	p = population
+	g = gravity(p, alpha, beta, gamma)
+	r = radiation(p)
+	o = opportunities(p, gamma)
 
-plt.xlabel(r'$ \frac{r}{\sqrt{N}}$')
-plt.ylabel('$p_{ij}$')
-plt.legend()
-plt.title('Log-log plot')
+	for i in range(p.size):
+		for j in range(p.size):
+			if i != j:
+				distance.append(p.r(i, j))
+
+				if model == 'all':
+					flux_gravity.append(g.flux(i, j))
+					flux_radiation.append(r.flux(i, j))
+					flux_opportunities.append(o.flux(i, j))
+
+				if isinstance(model, gravity) == True:
+					flux_gravity.append(g.flux(i, j))
+
+				if isinstance(model, radiation) == True:
+					flux_radiation.append(r.flux(i, j))
+
+				if isinstance(model, opportunities) == True:
+					flux_opportunities.append(o.flux(i, j))
+
+	plt.xlabel(r'$ \frac{r}{\sqrt{N}}$')
+	plt.ylabel('$p_{ij}$')
+	plt.legend()
+	plt.title('Log-log plot')
