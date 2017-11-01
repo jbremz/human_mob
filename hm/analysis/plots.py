f8d1e6282abe163985f30e770fbaf7dcfbbf817d
ilaria-manco <32579635+ilaria-manco@users.noreply.github.com>
2017-11-01 12:32:54 +0000

    better plot of flux vs distance

--- hm/analysis/plots.py
+++ hm/analysis/plots.py
@@ -1,14 +1,10 @@
-import importlib
 from hm.hm_models.gravity import gravity
 from hm.hm_models.radiation import radiation
 from hm.hm_models.opportunities import opportunities
 from hm.pop_models.pop_random import random as pop_random
-from hm.pop_models.pop_explicit import explicit as pop_explicit
 from matplotlib import pyplot as plt
 import numpy as np
 
-popDist = [3,4,7,5,6]
-locCoords = [[2,3],[3,2],[-5,9],[0,1],[1,-8]]
 alpha, beta = 1, 1
 gamma = 0.2
 N = 30
@@ -25,11 +21,16 @@ flux_opportunities = []
 for i in range(p.size):
 	for j in range(p.size):
 		if i != j:
-			distance.append(p.r(i, j))
+			distance.append(p.r(i, j)/np.sqrt(N))
 			flux_gravity.append(g.flux(i, j))
 			flux_radiation.append(r.flux(i, j))
 			flux_opportunities.append(o.flux(i, j))
 
-plt.loglog(distance, flux_gravity, 'r.')
-plt.loglog(distance, flux_radiation, 'b.')
-plt.loglog(distance, flux_opportunities, 'g.')
+plt.loglog(distance, flux_gravity, '.',markersize = 3, label = 'Gravity')
+plt.loglog(distance, flux_radiation, '.', markersize = 3,label = 'Radiation')
+plt.loglog(distance, flux_opportunities, '.', markersize = 3, label = 'Opportunities')
+
+plt.xlabel(r'$ \frac{r}{\sqrt{N}}$')
+plt.ylabel('$p_{ij}$')
+plt.legend()
+plt.title('Log-log plot')
