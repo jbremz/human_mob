eee6230913cb0a6f45a64dbe27724551c63f2928
jbremz <jhbremner@gmail.com>
2017-11-07 12:39:19 +0000

    Added radiation explicit tripoint

--- hm/analysis/explicit_tripoint.py
+++ hm/analysis/explicit_tripoint.py
@@ -1,5 +1,6 @@
 from hm.pop_models.pop_random import random as pop_random
 from hm.hm_models.gravity import gravity
+from hm.hm_models.radiation import radiation
 from hm.utils.utils import plot_pop
 import numpy as np
 import seaborn as sns; sns.set()
@@ -7,16 +8,9 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import FormatStrFormatter
 import copy
 
-def epsilon(x,y,N, size=1., ib=True, seed=False, tildeM=False, gamma=2):
+def createPops(x,y,N,size,seed):
 	'''
-	Takes the x and y displacements defined in the tripoint problem and returns
-	the error between treating the satellite locations as one and as separate
-
-	ib = True --> calculate Tib 
-	ib = False --> calulate Tbi (i.e. flow in opposite direction)
-
-	seed = False --> use completely random pop dist
-	seed = int --> recreate a previous population distribution
+	Returns the tripoint and two-point population objects as well as location of i and location of b
 
 	'''
 	if type(seed) is bool:
@@ -36,34 +30,95 @@ def epsilon(x,y,N, size=1., ib=True, seed=False, tildeM=False, gamma=2):
 	p3.locCoords = np.insert(p3.locCoords, 0, np.array([loci, locj, lock]), axis=0)
 	p3.popDist = np.insert(p3.popDist, 0, np.array([size, size, size]), axis=0)
 
-	g3 = gravity(p3, 1, 1, gamma) 
+	return p2, p3, loci, locb
+
+def tilde_m(size, mobObj):
+	'''
+	Takes pop size and mobility model object and returns the corrected tilde m for the size
+
+	# TODO: change to non-uniform size
+	'''
+	return 2*size - size*(mobObj.flux(1,2) - mobObj.flux(2,1))
+
+def epsilon(mobObj2, mobObj3, ib=True):
+	'''
+	Takes two-point and tri-point mobility model objects and returns epsilon
+
+	ib = True --> calculate Tib 
+	ib = False --> calulate Tbi (i.e. flow in opposite direction)
+
+	'''
+
+	if ib: # From i to the satellites
+		mob2Flux = mobObj2.flux(0,1)
+		eps = (mob2Flux - (mobObj3.flux(0,1)+mobObj3.flux(0,2)))/mob2Flux
+
+	else: # from the satellites to i 
+		mob2Flux = mobObj2.flux(1,0)
+		eps = (mob2Flux - (mobObj3.flux(1,0)+mobObj3.flux(2,0)))/mob2Flux
+
+	return eps
+
+
+def epsilon_g(x,y,N, size=1., ib=True, seed=False, tildeM=False, gamma=2):
+	'''
+	Takes the x and y displacements defined in the tripoint problem and returns
+	the error between treating the satellite locations as one and as separate
+
+	seed = False --> use completely random pop dist
+	seed = int --> recreate a previous population distribution
+
+	'''
+	p2, p3, loci, locb = createPops(x,y,N,size,seed)
+
+	g3 = gravity(p3, alpha=1, beta=1, gamma=gamma)
 
 	# Use definition of m_b with correction for the intra-location flow
-	# TODO: not sure about the motivation behind this definition + change to non-uniform size
 	if tildeM:
-		sizeb = 2*size - size*(g3.flux(1,2) - g3.flux(2,1))
+		sizeb = tilde_m(size, g3)
 	# use traditional definition of population mass m_b
 	else:
 		sizeb = 2*size 
 
+	# Insert locations into two-point
 	p2.popDist = np.insert(p2.popDist, 0, np.array([size, sizeb]), axis=0)
 	p2.locCoords = np.insert(p2.locCoords, 0, np.array([loci, locb]), axis=0)
 
 	g2 = gravity(p2, 1, 1, gamma)
 
-	if ib: # From i to the satellites
-		g2Flux = g2.flux(0,1)
-		eps = (g2Flux - (g3.flux(0,1)+g3.flux(0,2)))/g2Flux
-
-	else: # from the satellites to i 
-		g2Flux = g2.flux(1,0)
-		eps = (g2Flux - (g3.flux(1,0)+g3.flux(2,0)))/g2Flux
+	eps = epsilon(g2, g3, ib=ib)
 
 	return eps
 
 
+def epsilon_r(x,y,N, size=1., ib=True, seed=False, tildeM=False):
+	'''
+	Epsilon for radiation model
 
-def anaTP(xmin, xmax, ymin, ymax, n, N, ib=True, heatmap=True, tildeM=False):
+	'''
+	p2, p3, loci, locb = createPops(x,y,N,size,seed)
+
+	r3 = radiation(p3)
+
+	# Use definition of m_b with correction for the intra-location flow
+	if tildeM:
+		sizeb = tilde_m(size, r3)
+	# use traditional definition of population mass m_b
+	else:
+		sizeb = 2*size 
+
+	# Insert locations into two-point
+	p2.popDist = np.insert(p2.popDist, 0, np.array([size, sizeb]), axis=0)
+	p2.locCoords = np.insert(p2.locCoords, 0, np.array([loci, locb]), axis=0)
+
+	r2 = radiation(p2)
+
+	eps = epsilon(r2, r3, ib=ib)
+
+	return eps
+
+
+def anaTP(xmin, xmax, ymin, ymax, n, N, ib=True, heatmap=True, tildeM=False, func=gravity):
 	'''
 	Finds values of epsilon in an nxn 2D sample space for x and y at fixed N random locations and plots a heatmap
 	ib is a boolean to consider the direction of flow (see docstring for epsilon)
@@ -82,7 +137,7 @@ def anaTP(xmin, xmax, ymin, ymax, n, N, ib=True, heatmap=True, tildeM=False):
 
 	for j, row in enumerate(xy): # fill sample space
 		for i, pair in enumerate(row):
-			epsVals[j][i] = abs(epsilon(pair[0], pair[1], N, ib=ib, seed=seed, tildeM=tildeM))
+			epsVals[j][i] = abs(epsilon_r(pair[0], pair[1], N, ib=ib, seed=seed, tildeM=tildeM))
 
 	nanMask = np.isnan(epsVals)
 
