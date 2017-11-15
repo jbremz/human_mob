86e3b26b81f7e9bb60e18395eeaa9565c6ce1fad
ilaria-manco <32579635+ilaria-manco@users.noreply.github.com>
2017-11-15 12:41:22 +0000

    average of simulated values

--- hm/analysis/random_tripoint.py
+++ hm/analysis/random_tripoint.py
@@ -1,14 +1,10 @@
 from hm.hm_models.gravity import gravity
-from hm.pop_models.pop_random import random as pop_random
 from hm.hm_models.radiation import radiation
 from hm.hm_models.opportunities import opportunities
 import numpy as np
-import seaborn as sns; sns.set()
 import matplotlib.pyplot as plt
 import copy
 from hm.utils.utils import disp
-from scipy.interpolate import griddata
-from itertools import combinations
 from scipy import stats
 
 
@@ -60,6 +56,7 @@ def r_ib(p, i):
 	target location i as a Numpy array.
 	The pair's index matches that of neighbours().
 	'''
+
 	r = []
 	for n in neighbours(p, i):
 		x = (p.locCoords[n[0]][0]+ p.locCoords[n[1]][0])*0.5
@@ -77,14 +74,16 @@ def epsilon(p, i, model, tilde = False):
 		j, k = n[0], n[1]
 		p2 = copy.deepcopy(p)
 
-		#move j to midpoint
+		# move j to midpoint
 		p2.locCoords[j][0] = 0.5*(p2.locCoords[j][0]+ p2.locCoords[k][0])
 		p2.locCoords[j][1] = 0.5*(p2.locCoords[j][1]+ p2.locCoords[k][1])
 
-		#if tilde == True:
-		#	p2.popDist[j] = p2.popDist[k] + p2.popDist[j] - p2.popDist[j]*g.flux(j, k) - p2.popDist[k]*g.flux(j, k)
-		#merge two populations
-		p2.popDist[j] = p2.popDist[k] + p2.popDist[j]
+		if tilde == True:
+			# use m tilde correction
+			p2.popDist[j] = p2.popDist[k] + p2.popDist[j] - model.flux(j, k) - model.flux(k, j)
+		if tilde == False:
+			# merge two populations
+			p2.popDist[j] = p2.popDist[k] + p2.popDist[j]
 
 		p2.popDist[k] = 0. #remove k
 		b = j #rename j
@@ -95,8 +94,6 @@ def epsilon(p, i, model, tilde = False):
 			gamma = model.gamma
 
 			g2 = gravity(p2, alpha, beta, gamma, exp=True)
-			#if disp(p.locCoords[j], p2.locCoords[k]) < 0.07:
-			#if disp(p.locCoords[i], p2.locCoords[j]) > disp(p.locCoords[j], p2.locCoords[k]):
 			eps = (g2.flux(i, b) - (model.flux(i, j)+model.flux(i, k)))/(g2.flux(i, b))
 
 		if isinstance(model, radiation):
@@ -114,19 +111,27 @@ def epsilon(p, i, model, tilde = False):
 
 def eps_vs_neighbours(p, model, tilde = False):
 	'''
-	Returns two arrays with r_jk between all possible target-pair pairs
-	 and the corresponding epsilon.
+	Returns two arrays with r_jk between all possible target-pair pairs (sorted
+	in ascending order) and the corresponding epsilon.
 	'''
-	y = []
+
 	x = []
+	y = []
 	for i in range(p.size):
-		y.append(epsilon(p, i, model, tilde))
 		x.append(r_jk(p, i))
+		y.append(epsilon(p, i, model, tilde))
 	x = np.concatenate(x)
 	y = np.concatenate(y)
+	xy = np.array([x, y])
+	xy = xy[:,np.argsort(xy[0])]
 	return x, y
 
 def eps_vs_target(p, model, tilde = False):
+	'''
+	Returns two arrays with all possible r_ib (sorted in ascending order) and
+	the corresponding epsilon.
+	'''
+
 	y = []
 	x = []
 	for i in range(p.size):
@@ -140,9 +145,10 @@ def eps_vs_target(p, model, tilde = False):
 
 def eps_rib(p, model,r_jk, tilde = False):
 	'''
-	Returns the analytical form of epsilon for the exponential gravity model
-	as a function of r_ib.
+	Returns the analytical form of epsilon as a function of r_ib for the
+	exponential gravity model.
 	'''
+
 	if isinstance(model, gravity):
 		if model.exp:
 			x = eps_vs_target(p, model, tilde)[0]
@@ -150,17 +156,37 @@ def eps_rib(p, model,r_jk, tilde = False):
 				#only if m = 1 for all!
 				gamma = model.gamma
 				eps_values = 1 - (np.exp(-gamma*(np.sqrt(x**2 + (r_jk/2)**2)-x)))
-				#eps_values = 1-(1-np.arctan(r_jk/(2*x))/(np.pi))*np.exp(model.gamma*(x-np.sqrt(x**2 + (r_jk/2)**2)))
+				#eps_values = 1-(1-(np.arctan(r_jk/(2*x)))/(np.pi))*np.exp(model.gamma*(x-np.sqrt(x**2 + (r_jk/2)**2)))
 			return eps_values
 
-def r_jk_plot(p, model, r_jk, r_ib, tilde = False):
+def eps_rjk(p, model,r_ib, tilde = False):
+	'''
+	Returns the analytical form of epsilon as a function of r_jk for the
+	exponential gravity model.
+	'''
+
+	if isinstance(model, gravity):
+		if model.exp:
+			x = eps_vs_neighbours(p, model, tilde)[0]
+			for i in x:
+				#only if m = 1 for all!
+				gamma = model.gamma
+				eps_values = 1 - (np.exp(-gamma*(np.sqrt(r_ib**2 + (x/2)**2)-r_ib)))
+				#eps_values = 1-(1-(np.arctan(r_jk/(2*x)))/(np.pi))*np.exp(model.gamma*(x-np.sqrt(x**2 + (r_jk/2)**2)))
+			return eps_values
+
+def r_jk_plot(p, model, r_ib, tilde = False):
+	'''
+	Plots epsilon as a function of r_jk, given a costant value of r_ib.
+	'''
 	x = eps_vs_neighbours(p, model, tilde)[0]
 	y = eps_vs_neighbours(p, model, tilde)[1]
+	step = int(p.size/2)
 	plt.plot(x*np.sqrt(p.size), y, '.', label = 'simulation')
 	if isinstance(model, gravity):
-		plt.plot(x*np.sqrt(p.size), eps_rjk(p, model, r_jk, r_ib), '.', label = 'theory')
+		plt.plot(x*np.sqrt(p.size), eps_rjk(p, model, r_ib), '.', label = 'theory')
 		linregr = stats.linregress(x, y)
-		plt.plot(x*np.sqrt(p.size), x*np.sqrt(p.size)*linregr[0] +linregr[1], '.', label = 'regression')
+		#plt.plot(x*np.sqrt(p.size), x*np.sqrt(p.size)*linregr[0] +linregr[1], '.', label = 'regression')
 		plt.legend()
 	plt.xlabel('$\~r_{jk}$')
 	plt.ylabel('$\epsilon$')
@@ -168,18 +194,29 @@ def r_jk_plot(p, model, r_jk, r_ib, tilde = False):
 
 def r_ib_plot(p, model, r_jk, tilde = False):
 	'''
-	Returns plot of epsilon as a function of r_ib, given a costant value of r_jk.
+	Plots epsilon as a function of r_ib, given a costant value of r_jk.
 	'''
 
 	x = eps_vs_target(p, model, tilde)[0, :]
 	y = eps_vs_target(p, model, tilde)[1,:]
-	for i in np.arange(0, len(x), 50):
-		mean_y = np.mean(y[i:i+50])
-		plt.plot(x[i]*np.sqrt(p.size), mean_y, '.')
+	step = int(p.size/2)
+	for i in np.arange(0, len(x), step):
+		mean_y = np.mean(y[i:i+step])
+		plt.plot(x[i]*np.sqrt(p.size), mean_y, '.',)
 	#plt.plot(x*np.sqrt(p.size), y, '.', label = 'simulation')
 	if isinstance(model, gravity):
 		plt.plot(x*np.sqrt(p.size), eps_rib(p, model, r_jk), '.', label = 'theory')
 	plt.xlabel('$\~r_{ib}$')
 	plt.ylabel('$\epsilon$')
 	plt.legend()
+	plt.show()
+
+def test_ratio(p, model):
+	#delta_theta = 2*np.arctan(r_jk/(2*r_ib))
+	#integral = ((2*np.pi- delta_theta)/model.gamma**2) * (model.gamma*np.sqrt(1./p.size))
+	r_ij = np.arange(0., 1., 0.01)
+	r_jk = 0.1
+	r_ib = np.sqrt(r_ij**2 - (r_jk/2.)**2)
+	ratio = (np.exp(-model.gamma*r_ij))/(np.exp(-model.gamma*r_ib))
+	plt.plot(r_ij, ratio)
 	plt.show()
