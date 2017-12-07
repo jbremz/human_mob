ae0af126ee0b52635b5bc0f0f3d17bf0d19201d3
ilaria-manco <32579635+ilaria-manco@users.noreply.github.com>
2017-12-07 10:14:00 +0000

    changes data path

--- hm/analysis/mexico_data.py
+++ hm/analysis/mexico_data.py
@@ -2,7 +2,7 @@ import gdal
 import numpy as np
 
 
-data = gdal.Open("hrsl_mex.tif")
+data = gdal.Open("/Users/Ilaria/Documents/Imperial/MSci_Project/Datasets/hrsl_mex_v1/hrsl_mex.tif")
 
 for n in range(data.RasterCount):
 	band = data.GetRasterBand(n+1)
