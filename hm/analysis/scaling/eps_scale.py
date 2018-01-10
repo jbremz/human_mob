9bcf445aa2f3f5b5ed10b0b3097ac3c5e03d4755
jbremz <jhbremner@gmail.com>
2018-01-10 15:29:34 +0000

    trimming

--- hm/analysis/scaling/eps_scale.py
+++ hm/analysis/scaling/eps_scale.py
@@ -7,26 +7,22 @@ from hm.hm_models.radiation import radiation
 from hm.coarse_grain.coarse_matrix import epsilon_matrix, reorder_matrix
 from hm.coarse_grain.coarse_matrix import coarse_grain as coarse_grain_matrix
 
-df = pandas.read_csv("/Users/Ilaria/Documents/Imperial/MSci_Project/Code/human_mob/data/CDRC/england_welsh_scotland_pwc2011.csv")
-lon_df = df[df['Easting'] < 561966][df['Easting'] > 493024][df['Northing'] < 207218][df['Northing'] > 150912]
-
 def iterate(df, d_max, times = 0):
 	"""Returns a list of Clusters objects with all the levels up to level = times."""
-	x = np.array(df)[:,5]
-	y = np.array(df)[:,6]
-	m = np.array(df)[:,2]
-	xy = np.array([x, y])
-	xy = np.transpose(xy)
+	x = np.array(df['Easting'])
+	y = np.array(df['Northing'])
+	m = np.array(df['TotPop2011'])
+	xy = np.array([x, y]).T
 	pop = pop_explicit(xy, m)
 	clusters = Clusters(pop, d_max)
 	levels = [clusters]
 	
 	if times > 0:
 		for i in range(times):
-			xy = np.transpose(clusters.centroids())
+			xy = clusters.centroids().T
 			m = clusters.clustered_pop
 			pop = pop_explicit(xy, m)
-			d_max = d_max + 2000
+			d_max = d_max + 2000 # TODO maybe change this to a scaling factor e.g. 2*d_max?
 			clusters = Clusters(pop, d_max)
 			levels.append(clusters)
 	
@@ -36,7 +32,7 @@ def gravity_ODM(df, d_max, level, gamma = 0.2):
 	"""Returns the ODM for the gravity model at a specific level of clustering."""
 	clustering = iterate(df, d_max, times = level)
 	m = clustering[-1].clustered_pop
-	xy = np.transpose(clustering[-1].centroids())
+	xy = clustering[-1].centroids().T
 	pop = pop_explicit(xy, m)
 	g = gravity(pop, 1, 1, gamma)
 	return g.ODM()
@@ -45,17 +41,16 @@ def reordered_ODM(df, d_max, level, gamma = 0.2):
 	"""Returns ODM for the flow between clustered locations."""
 	original_ODM = gravity_ODM(df, d_max, level-1, gamma)
 	clust = iterate(df, d_max, level)[level].clusters
-	reordered_ODM = reorder_matrix(original_ODM, clust)[0]	
+	reordered_ODM = reorder_matrix(original_ODM, clust)[0]
 	return reordered_ODM
 
 def reduced_ODM(df, d_max, level, gamma = 0.2):
 	"""Returns ODM for the combined flow between locations."""
 	if level == 0:
-		x = np.array(df)[:,5]
-		y = np.array(df)[:,6]
-		m = np.array(df)[:,2]
-		xy = np.array([x, y])
-		xy = np.transpose(xy)
+		x = np.array(df['Easting'])
+		y = np.array(df['Northing'])
+		m = np.array(df['TotPop2011'])
+		xy = np.array([x, y]).T
 		pop = pop_explicit(xy, m)
 		original_ODM = gravity(pop, 1, 1, gamma).ODM()
 	else:
@@ -68,5 +63,6 @@ def epsilon(df, d_max, level, gamma = 0.2):
 	"""Retures the epsilon matrix at a specific level of clustering."""
 	clustered_ODM = gravity_ODM(df, d_max, level, gamma)
 	combined_ODM = reduced_ODM(df, d_max, level, gamma)
-	epsilon = epsilon_matrix(clustered_ODM, combined_ODM)
+	epsilon = epsilon_matrix(combined_ODM, clustered_ODM) # WARNING changed this round
 	return epsilon
+
