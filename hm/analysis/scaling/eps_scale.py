d267d655f5fdfd112caaee47c4af6701a3d2e9e5
ilaria-manco <32579635+ilaria-manco@users.noreply.github.com>
2018-01-17 17:28:15 +0000

    deleted unused ODM functions

--- hm/analysis/scaling/eps_scale.py
+++ hm/analysis/scaling/eps_scale.py
@@ -3,26 +3,23 @@ from hm.coarse_grain.clustering import Clusters
 from hm.pop_models.pop_explicit import explicit as pop_explicit
 from hm.hm_models.gravity import gravity
 from hm.hm_models.radiation import radiation
-from hm.coarse_grain.coarse_matrix import epsilon_matrix, reorder_matrix
+from hm.coarse_grain.coarse_matrix import epsilon_matrix
 from hm.coarse_grain.coarse_matrix import coarse_grain as coarse_grain_matrix
-from matplotlib import pyplot as plt
 from hm.analysis.scaling.pop_tools import make_pop
 
-def iterate(df, d_max, level = 1):
+def iterate(df, d_max):
 	"""Returns a list of Clusters objects with all the levels up to specified level."""
 
 	# Clustering starts at level 1 (level 0 is unclustered)
-	if level < 1:
+	if len(d_max) < 1:
 		print("iterate() only accepts clustering levels > 0")
 		return
 
 	pop = make_pop(df)
-	clusters = Clusters(pop, d_max)
-	levels = [clusters]
+	levels = []
 
-	for i in range(level-1):
-		d_max = d_max + 2000 # TODO maybe change this to a scaling factor e.g. 2*d_max?
-		clusters = Clusters(pop, d_max)
+	for i in d_max:
+		clusters = Clusters(pop, i)
 		levels.append(clusters)
 		
 	return levels		
@@ -37,78 +34,47 @@ def cluster_population(cluster, pw = True):
 	""" 
 	if not isinstance(cluster, Clusters):
 		raise NameError("cluster must be a Clusters object")
+		
+	m = cluster.clustered_pop
+	if pw == True:
+		xy = cluster.pw_centroids().T
 	else:
-		m = cluster.clustered_pop
-		if pw == True:
-			xy = cluster.pw_centroids().T
-		else:
-			xy = cluster.centroids().T
-		pop = pop_explicit(xy, m)
+		xy = cluster.centroids().T
+	pop = pop_explicit(xy, m)
+	
 	return pop
 
-def gravity_ODM(df, d_max, level, gamma = 0.2):
-	"""Returns the ODM for the gravity model at a specific level of clustering."""
+def gravity_ODM(clusters_list, level, gamma, df = 1):
+	"""
+	Returns the ODM for the gravity model at a specific level of clustering.
+	
+	If level = 0 (no clustering), a dataframe needs to be specified so that 
+	a population object can be created from it.
+	"""
+	
 	if level == 0:
 		pop = make_pop(df)
-
-	else:
-		clustering = iterate(df, d_max, level = level)[-1]
+	
+	if level > 0:
+		clustering = clusters_list[level-1]
 		pop = cluster_population(clustering)
 
 	g = gravity(pop, 1, 1, gamma)
 	return g.ODM()
 
-def reordered_ODM(df, d_max, level, gamma = 0.2):
-	"""Returns reordered ODM for the flow between clustered locations."""
-	original_ODM = gravity_ODM(df, d_max, level-1, gamma)
-	clust = iterate(df, d_max, level)[level-1].clusters
-	reordered_ODM = reorder_matrix(original_ODM, clust)[0]
-	return reordered_ODM
-
-def reduced_ODM(df, d_max, level, gamma = 0.2):
+def reduced_ODM(clusters_list, level, gamma, df):
 	"""Returns ODM for the combined flow between locations."""
 	if level != 0:
-		original_ODM = gravity_ODM(df, d_max, level-1, gamma)
-		clust = iterate(df, d_max, level)[level-1].clusters
+		original_ODM = gravity_ODM(clusters_list, 0, gamma, df)
+		clust = clusters_list[level-1].clusters
 		reduced_ODM = coarse_grain_matrix(original_ODM, clust)
 	else: # not reduced
-		reduced_ODM = gravity_ODM(df, d_max, level, gamma)
+		reduced_ODM = gravity_ODM(clusters_list, level, gamma, df)
 	return reduced_ODM
 
-def multi_reduced_ODM(df, d_max, level, gamma = 0.2):
-	"""Recursively combines flows at each clustering level and returns the final combined ODM"""
-
-	# Original populations
-	pop = make_pop(df)
-	original_ODM = gravity(pop, 1, 1, gamma).ODM()
-
-	ODM_init = original_ODM
-
-	if level != 0:
-		levels = iterate(df, d_max, level) # list of clusters at each level
-		# Recursively reducing
-		for i in range(level):
-			ODM_init = coarse_grain_matrix(ODM_init, levels[i].clusters)
-
-	return ODM_init
-
-def epsilon(df, d_max, level, gamma = 0.2):
-
-	"""Returns the epsilon matrix at a specific level of clustering."""
-	clustered_ODM = gravity_ODM(df, d_max, level, gamma)
-	combined_ODM = reduced_ODM(df, d_max, level, gamma)
-	epsilon = epsilon_matrix(combined_ODM, clustered_ODM) # WARNING changed this round
-	return epsilon
-
-def epsilonB(df, d_max, level, gamma = 0.2):
+def epsilon(clusters_list, level, gamma, df):
 	"""Returns the epsilon matrix (defined compared to the ODM at the original location resolution) at a specific level of clustering."""
-	clustered_ODM = gravity_ODM(df, d_max, level, gamma)
-	combined_ODM = multi_reduced_ODM(df, d_max, level, gamma)
+	clustered_ODM = gravity_ODM(clusters_list, level, gamma, df)
+	combined_ODM = reduced_ODM(clusters_list, level, gamma, df)
 	epsilon = epsilon_matrix(combined_ODM, clustered_ODM)
 	return epsilon
-
-
-
-
-
-
