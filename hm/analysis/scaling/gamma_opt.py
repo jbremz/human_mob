72aae9ebef7402a24c05ffc498c8e77d2c5df3a5
ilaria-manco <32579635+ilaria-manco@users.noreply.github.com>
2018-02-13 18:24:24 +0000

    notebook week 16

--- hm/analysis/scaling/gamma_opt.py
+++ hm/analysis/scaling/gamma_opt.py
@@ -2,20 +2,20 @@ import numpy as np
 from hm.analysis.scaling.pop_hierarchy import pop_hier
 from scipy.optimize import minimize_scalar
 
-def optimise_eps(hier, level, gamma_0):
+def optimise_eps(hier, level, gamma_0, exp = False):
 	# if not isinstance(hier, pop_hier):
 	# 	raise NameError("hier must be an object of the pop_hier class!")
 	
 	def mean(gamma):
-		return mean_eps(hier, level, gamma_0, gamma)
+		return mean_eps(hier, level, gamma_0, gamma, exp=exp)
 	
 	#array = np.array([mean(gamma_0), mean(gamma_0+1)])
 	opt = minimize_scalar(mean, bounds=(gamma_0-3, gamma_0+3), tol = 0.001)
 	
 	return opt
 		
-def mean_eps(hier, level, gamma_0, gamma):
-	eps = hier.epsilon_to_opt(level, gamma_0, gamma)
+def mean_eps(hier, level, gamma_0, gamma, exp=False):
+	eps = hier.epsilon_to_opt(level, gamma_0, gamma, exp=exp)
 	N = len(eps)
 	grid = np.indices((N, N))
 	diag = grid[0] == grid[1]
