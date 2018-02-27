a77f753c601ac362099139cb4bee7eaf3385900b
ilaria-manco <32579635+ilaria-manco@users.noreply.github.com>
2018-02-27 08:31:07 +0000

    gamma opt for whole London (fixed)

--- hm/analysis/scaling/gamma_opt.py
+++ hm/analysis/scaling/gamma_opt.py
@@ -1,6 +1,9 @@
 import numpy as np 
 from hm.analysis.scaling.pop_hierarchy import pop_hier
 from scipy.optimize import minimize_scalar
+from scipy.optimize import leastsq
+from scipy.optimize import minimize
+
 
 def optimise_eps(hier, level, gamma_0, exp = False):
 	# if not isinstance(hier, pop_hier):
@@ -10,7 +13,9 @@ def optimise_eps(hier, level, gamma_0, exp = False):
 		return mean_eps(hier, level, gamma_0, gamma, exp=exp)
 	
 	#array = np.array([mean(gamma_0), mean(gamma_0+1)])
-	opt = minimize_scalar(mean, bounds=(gamma_0-3, gamma_0+3), tol = 0.001)
+	#opt = leastsq(mean, gamma_0)
+	opt = minimize(mean, gamma_0, tol = 0.001)
+	#opt = minimize_scalar(mean, bounds=(gamma_0-3, gamma_0+3), tol = 0.001)
 	
 	return opt
 		
