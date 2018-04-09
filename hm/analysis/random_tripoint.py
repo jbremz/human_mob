99de7e3c298a93c84a94779ce77914a097f83393
ilaria-manco <32579635+ilaria-manco@users.noreply.github.com>
2018-04-09 13:45:20 +0100

    random tripoint - added exp

--- hm/analysis/random_tripoint.py
+++ hm/analysis/random_tripoint.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 import copy
 from hm.utils.utils import disp
 from sklearn.neighbors import NearestNeighbors
+from sklearn.metrics import mean_squared_error as mse
+
 
 def neighbours(p):
 	'''
@@ -118,14 +120,14 @@ def eps_vs_target(p, model, exp=True, tilde = False):
 	xy = xy[:,np.argsort(xy[0])]
 	return xy
 
-def eps_rib(p, model,r_jk, tilde = False):
+def eps_rib(p, model,r_jk, exp=True, tilde = False):
 	'''
 	Returns the analytical form of epsilon as a function of r_ib for the
 	exponential gravity model.
 	'''
 
 	if isinstance(model, gravity):
-		x = eps_vs_target(p, model, tilde)[0]
+		x = eps_vs_target(p, model,exp=exp, tilde=tilde)[0]
 		r_ij = np.sqrt(x**2 + (r_jk/2)**2)
 		gamma = model.gamma
 		
@@ -139,99 +141,149 @@ def eps_rib(p, model,r_jk, tilde = False):
 			eps_values = 1 - (r_ij/x)**(-gamma)
 			
 	if isinstance(model, radiation):
-		x = eps_vs_target(p, model, tilde)[0]
+		x = eps_vs_target(p, model, exp=exp, tilde=tilde)[0]
 		for i in x:
 			#only if m = 1 for all!
 			eps_values = 1 - ((x**2)*(np.pi*p.size*x**2 + 2))/((x**2 + (r_jk/2.)**2)*(np.pi*p.size*(x**2 + (r_jk/2.)**2 )+1))
 	return eps_values
 
-def eps_rjk(p, model,r_ib, tilde = False):
+def eps_rjk(p, model,r_ib, exp=True, tilde = False):
 	'''
 	Returns the analytical form of epsilon as a function of r_jk for the
 	exponential gravity model.
 	'''
 
 	if isinstance(model, gravity):
-		if model.exp:
-			x = eps_vs_neighbours(p, model, tilde)[0]
-			for i in x:
-				#only if m = 1 for all!
-				gamma = model.gamma
-				eps_values = 1 - (np.exp(-gamma*(np.sqrt(r_ib**2 + (x/2)**2)-r_ib)))
-
-	if isinstance(model, radiation):
-		x = eps_vs_neighbours(p, model, tilde)[0]
+		x = eps_vs_neighbours(p, model,exp=exp, tilde=tilde)[0]
+		r_ij = np.sqrt(r_ib**2 + (x/2)**2)
+		
 		for i in x:
 			#only if m = 1 for all!
-			eps_values = 1 - ((r_ib**2)*(np.pi*p.size*r_ib**2 + 2))/((r_ib**2 + (x/2.)**2)*(np.pi*p.size*(r_ib**2 + (x/2.)**2 )+1))
+			gamma = model.gamma
+			if model.exp:
+				eps_values = 1 - (np.exp(-gamma*(r_ij - r_ib)))
+			else:
+				eps_values = 1 - (r_ij/r_ib)**(-gamma)
+			
+
+	if isinstance(model, radiation):
+		x = eps_vs_neighbours(p, model,exp=exp, tilde=tilde)[0]
+		r_ij = np.sqrt(r_ib**2 + (x/2)**2)
+		rho = p.size
+		for i in x:
+			#only if m = 1 for all!
+			eps_values = 1 - (r_ib**2/r_ij**2)*(np.pi * rho * r_ib**2 + 2)/(np.pi * rho * r_ij**2 + 1)
 
 	return eps_values
 
-def mean_r_ib(p, model, r_jk, exp=True, tilde = False):
-	eps_target = eps_vs_target(p, model, exp=exp, tilde=tilde)
-	x = eps_target[0, :]
-	y = eps_target[1,:]
-	step = int(len(y)/(p.size*2))
+def rjk_binning(p, model, exp):
+	x,y = eps_vs_neighbours(p, model, exp=exp)
 	mean_y = []
-	mean_x = []
 	std_y = []
-	for i in np.arange(0, int(len(x)/6), 40):
-		if i >= step:
-			mean_y.append(np.mean(y[i-step:i+step]))
-			std = np.std(y[i:i+int(step/2)])
-			std_y.append(std/np.sqrt(len(y[i:i+int(step/2)])))
-			mean_x.append(np.mean(x[i:i+int(step/2)]))
-		if i < step:
-			mean_y.append(np.mean(y[i:i+int(step/2)]))
-			std = np.std(y[i:i+int(step/2)])
-			std_y.append(std/np.sqrt(len(y[i:i+int(step/2)])))
-			mean_x.append(np.mean(x[i:i+int(step/2)]))
-			
-	for i in np.arange(int(len(x)/6), len(x), 50):
-		#used 50 for exp
-		if i >= step:
-			mean_y.append(np.mean(y[i-step:i+step]))
-			std = np.std(y[i:i+int(step/2)])
-			std_y.append(std/np.sqrt(len(y[i:i+int(step/2)])))
-			mean_x.append(np.mean(x[i:i+int(step/2)]))
-		if i < step:
-			mean_y.append(np.mean(y[i:i+int(step/2)]))
-			std = np.std(y[i:i+int(step/2)])
-			std_y.append(std/np.sqrt(len(y[i:i+int(step/2)])))
-			mean_x.append(np.mean(x[i:i+int(step/2)]))
-			
-	return x, mean_x, mean_y, std_y
+	mean_x, indices = np.unique(eps_vs_neighbours(p, model,exp=exp)[0], return_index=True)
+	for i in range(1, len(indices)+1):
+		if i == len(indices):
+			ran = list(range(indices[-1], len(x)))
+		else:
+			ran = list(range(indices[i-1], indices[i]+1))
+		mean_y.append(np.mean(y[ran[0]:ran[-1]]))
+		std_y.append(np.std(y[ran[0]:ran[-1]])/np.sqrt(len(y[ran[0]:ran[-1]])))
 
-def mean_r_jk(p, model, r_jk, tilde = False):
-	eps_neighbours = eps_vs_neighbours(p, model, tilde)
-	x = eps_neighbours[0, :]
-	y = eps_neighbours[1,:]
-	step = int(len(y)/p.size)
-	mean_y = []
-	for i in np.arange(0, len(x), 1):
-		if i >= step:
-			mean_y.append(np.mean(y[i-step:i+step]))
-		if i < step:
-			mean_y.append(np.mean(y[i:i+int(step/2)]))
-	return x, mean_y
+	return x, mean_x, mean_y, np.array(std_y)
+	
+def mean_r(p, model, x_value, exp=True, tilde = False):
+	if x_value == "r_ib":
+		eps_target = eps_vs_target(p, model, exp=exp, tilde=tilde)
+		x = eps_target[0, :]
+		y = eps_target[1,:]
+		
+		step = int(len(y)/(p.size*2))
+		mean_y = []
+		mean_x = []
+		std_y = []
+		for i in np.arange(0, int(len(x)/6), 40):
+			if i >= step:
+				mean_y.append(np.mean(y[i-step:i+step]))
+				std = np.std(y[i:i+int(step/2)])
+				std_y.append(std/np.sqrt(len(y[i:i+int(step/2)])))
+				mean_x.append(np.mean(x[i:i+int(step/2)]))
+			if i < step:
+				mean_y.append(np.mean(y[i:i+int(step/2)]))
+				std = np.std(y[i:i+int(step/2)])
+				std_y.append(std/np.sqrt(len(y[i:i+int(step/2)])))
+				mean_x.append(np.mean(x[i:i+int(step/2)]))
+				
+		for i in np.arange(int(len(x)/6), len(x), 50):
+			#used 50 for exp
+			if i >= step:
+				mean_y.append(np.mean(y[i-step:i+step]))
+				std = np.std(y[i:i+int(step/2)])
+				std_y.append(std/np.sqrt(len(y[i:i+int(step/2)])))
+			if i < step:
+				mean_y.append(np.mean(y[i:i+int(step/2)]))
+				std = np.std(y[i:i+int(step/2)])
+				std_y.append(std/np.sqrt(len(y[i:i+int(step/2)])))
+				mean_x.append(np.mean(x[i:i+int(step/2)]))
+		
+	if x_value == "r_jk":
+		if isinstance(model, gravity):
+			x, mean_x, mean_y, std_y = rjk_binning(p, model, exp=exp)
+			
+		if isinstance(model, radiation):
+			x, x_new, y, std_old = rjk_binning(p, model, exp=exp)
+			
+			step = int(len(y)/5)
+			mean_y = []
+			mean_x = []
+			std_y = []
+			
+			for i in np.arange(0, int(len(x)), 1):
+				if i >= step:
+					mean_y.append(np.mean(y[i-step:i+step]))
+					std = np.std(y[i:i+int(step/2)])
+					std_y.append(std/np.sqrt(len(y[i:i+int(step/2)])))
+					mean_x.append(np.mean(x_new[i:i+int(step/2)]))
+				#std_y = std_old + np.array(std_y)
+					
+				if i < step:
+					mean_y.append(np.mean(y[i:i+int(step/2)]))
+					std = np.std(y[i:i+int(step/2)])
+					std_y.append(std/np.sqrt(len(y[i:i+int(step/2)])))
+					mean_x.append(np.mean(x_new[i:i+int(step/2)]))
+
+	return x, np.array(mean_x), mean_y, std_y
 
 def plot_ratio(p, model, r_jk, collapse = False, tilde = False):
 	theory = eps_rib(p, model, r_jk)
-	mean = mean_r_ib(p, model, r_jk)
+	mean = mean_r(p, model,"r_ib")
 	x = mean[0]
 	mean_y = mean[1]
 	
 	plt.rcParams.update(plt.rcParamsDefault)
 	plt.style.use('seaborn-deep')
-
+	
+	# Resolution
+	fig = plt.figure(figsize=(800/110.27, 800/110.27), dpi = 500)
+	
 	if collapse == True:
 		plt.plot(x, mean_y/theory, '.', label = 'N = '+str(p.size))
 		plt.xlabel('$\~r_{ib}} / sqrt(N)} $')
 	else:
 		plt.plot(x*np.sqrt(p.size), mean_y/theory, '.', label = 'N = '+str(p.size))
 		plt.xlabel('$\~r_{ib}$')
-	plt.ylabel('ratio')
-	plt.legend()
+		
+	plt.ylabel('$\epsilon_{sim}$/$\epsilon_{ana}$', fontsize = 20)
+	# Legend
+	plt.legend(frameon=False, fontsize=20)
+	
+	# Axes/tick labels
+	plt.tick_params(axis='both', labelsize=15)
+	plt.ticklabel_format(style='sci')
+	
+	plt.tight_layout()
+	
+	plt.savefig('ratio')
+
 
 def plot_ratio_rjk(p, model, r_ib, tilde = False, collapse = False):
 	theory = eps_rjk(p, model, r_ib)
